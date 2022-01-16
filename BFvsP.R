c1c6c5cbf19b32fecafd0773a439f8364489b8f2
fbarraquand <frederic.barraquand@u-bordeaux.fr>
2022-01-16 12:15:20 +0100

    Improved legends and colors

--- BFvsP.R
+++ BFvsP.R
@@ -1,36 +1,35 @@
-
-
 library(BayesFactor)
-
-
-par(mfrow = c(1,2))
-
 
 getP <- function(n=10, effectSize = 0, sd = 1){
   x = rnorm(n, mean = effectSize, sd= sd) 
   return(t.test(x)$p.value)
 }
 
+pdf(file = "Fig1_1sampleTtest.pdf",width=10,height=5)
 
-plot(NULL, xlim = c(0,1), ylim = c(0,3), xlab = "p-value", ylab = "probabilty of observing p", main = "Expected p distribution")
+par(mfrow = c(1,2))
+
+plot(NULL, xlim = c(0,1), ylim = c(0,6), xlab = "p-value", ylab = "frequency of obs. of p", main = "p distribution")
 
 abline(h=1)
 
-alt1 = replicate(10000, getP(n=2, effectSize = 0.5))
+alt1 = replicate(10000, getP(n=15, effectSize = 0.2))
 x = hist(alt1, breaks = 20, plot = F)
 lines(x$mids, x$density, type = "both", pch = 1, col = "blue")
 
-
-alt2 = replicate(10000, getP(n=20, effectSize = 0.5))
+alt2 = replicate(10000, getP(n=100, effectSize = 0.2))
 x = hist(alt2, breaks = 20, plot = F)
-lines(x$mids, x$density, type = "both", pch = 2, col = "red")
+lines(x$mids, x$density, type = "both", pch = 0, col = "red")
+
+alt3 = replicate(10000, getP(n=200, effectSize = 0.2))
+x = hist(alt3, breaks = 20, plot = F)
+lines(x$mids, x$density, type = "both", pch = 2, col = "orange")
+
+legend("topright", legend = c("null", "n = 15, power = 10%", "n = 100, power = 50%","n = 200, power = 80%"), lty = c(1,NA,NA,NA), pch = c(NA,1,0,2), col = c("black", "blue", "red","orange"))
 
 
-legend("topright", legend = c("null", "underpowered", "properly powered"), pch = 1, col = c("black", "blue", "red"))
 
-
-
-getResult <- function(n=10, effectSize = 0.4, sd = 1){
+getResult <- function(n=10, effectSize = 0.2, sd = 1){
   x = rnorm(n, mean = effectSize, sd= sd) 
   p = t.test(x)$p.value
   lmod = lm(x ~ 1)
@@ -38,22 +37,30 @@ getResult <- function(n=10, effectSize = 0.4, sd = 1){
   #anova(lmod, lmod2)
   BF = extractBF(ttestBF(x))$bf
   odds = BF/(BF + 1)
-  return(c(p = p, oddsAlt = odds))
+  return(c(p = p, probAlt = odds))
 }
 
-sim1 = as.data.frame(t(replicate(500, getResult(n = 2))))
-sim2 = as.data.frame(t(replicate(500, getResult(n = 15))))
+sim1 = as.data.frame(t(replicate(500, getResult(n = 10))))
+sim2 = as.data.frame(t(replicate(500, getResult(n = 20))))
 sim3 = as.data.frame(t(replicate(500, getResult(n = 100))))
 
 
-plot(oddsAlt ~ p, data = sim1, ylim = c(0,1), xlim = c(0,1.1), ylab = "Odds in favor of the alternative", xlab = "p-value", main = "BF vs. p-value")
-points(oddsAlt ~ p, data = sim2, col = "red")
-points(oddsAlt ~ p, data = sim3, col = "blue")
+plot(probAlt ~ p, data = sim1, ylim = c(0,1), xlim = c(0,0.6), ylab = "Pr(H1|data)", xlab = "p-value", main = "BF vs. p-value")
+points(probAlt ~ p, data = sim2, col = "blue")
+points(probAlt ~ p, data = sim3, col = "red")
 
 abline(h=0.5, lty = 2)
 
-legend("topright", legend = c("n = 5", "n = 15", "n = 50"), pch = 1, col = c("black", "red", "blue"))
-text(0.5, 0.97, "!H0 more likely", adj = 0.5)
+legend("top", legend = c("n = 10", "n = 20", "n = 100"), pch = 1, col = c("black", "blue", "red"))
+text(0.5, 0.97, "H1 more likely", adj = 0.5)
 text(0.5, 0.03, "H0 more likely", adj = 0.5)
 
+dev.off()
+
+# Power of tests in the one-sample example
+power.t.test(n = 10, delta = 0.2, type="one.sample")
+power.t.test(n = 15, delta = 0.2, type="one.sample")
+power.t.test(n = 20, delta = 0.2, type="one.sample")
+power.t.test(n = 100, delta = 0.2, type="one.sample")
+power.t.test(n = 200, delta = 0.2, type="one.sample")
 
--- BFvsP_twosamples.R
+++ BFvsP_twosamples.R
@@ -1,15 +1,16 @@
 library(BayesFactor)
 
-par(mfrow = c(1,2))
-
 getP <- function(n=20, effectSize = 0, sd = 1){
   x = rnorm(n, mean = 0, sd= sd) 
   y = rnorm(n, mean = effectSize, sd= sd)
   return(t.test(x,y)$p.value)
 }
 
+pdf(file = "Fig1_2sampleTtest.pdf",width=10,height=5)
 
-plot(NULL, xlim = c(0,1), ylim = c(0,3), xlab = "p-value", ylab = "probability of observing p", main = "Expected p distribution")
+par(mfrow = c(1,2))
+
+plot(NULL, xlim = c(0,1), ylim = c(0,6), xlab = "p-value", ylab = "frequency of obs. of p", main = "p distribution")
 
 abline(h=1)
 
@@ -21,10 +22,7 @@ alt2 = replicate(10000, getP(n=50, effectSize = 0.5))
 x = hist(alt2, breaks = 20, plot = F)
 lines(x$mids, x$density, type = "both", pch = 2, col = "red")
 
-
-legend("topright", legend = c("null", "n=10", "n=50"), pch = 1, col = c("black", "blue", "red"),cex=0.5)
-
-
+legend("topright", legend = c("null", "n = 10, power = 18%", "n = 50, power = 70%"), lty = c(1,NA,NA), pch = c(NA,1,2), col = c("black", "blue", "red"),cex=1)
 
 getResult <- function(n=10, effectSize = 0.5, sd = 1,rscale="medium"){
   x = rnorm(n, mean = 0, sd= sd) 
@@ -54,28 +52,25 @@ plot(probAlt ~ p, data = sim1, ylim = c(0,1), xlim = c(0,0.6), ylab = "Pr(H1|dat
 points(probAlt ~ p, data = sim2, col = "red")
 points(probAlt ~ p, data = sim3, col = "violet")
 
-points(probAlt ~ p, data = sim4, col = "black",pch=15)
-points(probAlt ~ p, data = sim5, col = "red",pch=15)
-points(probAlt ~ p, data = sim6, col = "violet",pch=15)
+points(probAlt ~ p, data = sim4, col = "black",pch=20)
+points(probAlt ~ p, data = sim5, col = "red",pch=20)
+points(probAlt ~ p, data = sim6, col = "violet",pch=20)
 
 
 abline(h=0.5, lty = 2)
 
-legend("topright", legend = c("n = 20", "n = 50", "n = 100"), pch = 1, col = c("black", "red", "violet"),cex=0.5)
+legend("top", legend = c("n = 20", "n = 50", "n = 100"), pch = 1, col = c("black", "red", "violet"),cex=1)
 text(0.5, 0.97, "H1 more likely", adj = 0.5)
 text(0.5, 0.03, "H0 more likely", adj = 0.5)
+
+dev.off()
+
 
 # Power of tests considered here
 power.t.test(n = 10, delta = 0.5, type="two.sample")
 power.t.test(n = 20, delta = 0.5, type="two.sample")
 power.t.test(n = 50, delta = 0.5, type="two.sample")
 power.t.test(n = 100, delta = 0.5, type="two.sample")
-
-# Power of tests in the previous one-sample example
-power.t.test(n = 2, delta = 0.5, type="one.sample")
-power.t.test(n = 15, delta = 0.5, type="one.sample")
-power.t.test(n = 20, delta = 0.5, type="one.sample")
-power.t.test(n = 100, delta = 0.5, type="one.sample")
 
 # Get back to the bayes factor
 meanBF<-function(sim){mean(sim$probAlt/(1-sim$probAlt))}
