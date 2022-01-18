103eb8343c983da68343336b414cf86babb1990a
Florian Hartig <florianhartig@users.noreply.github.com>
2022-01-18 10:29:56 +0100

    updates

--- BFvsP.R
+++ BFvsP.R
@@ -5,27 +5,31 @@ getP <- function(n=10, effectSize = 0, sd = 1){
   return(t.test(x)$p.value)
 }
 
-pdf(file = "Fig1_1sampleTtest.pdf",width=10,height=5)
+pdf(file = "Fig1_1sampleTtest.pdf",width=9,height=5)
 
 par(mfrow = c(1,2))
 
-plot(NULL, xlim = c(0,1), ylim = c(0,6), xlab = "p-value", ylab = "frequency of obs. of p", main = "p distribution")
+plot(NULL, xlim = c(0,1), ylim = c(0,6), xlab = "p-value", 
+     ylab = "frequency", main = "Simulated p-distribution")
 
 abline(h=1)
 
-alt1 = replicate(10000, getP(n=15, effectSize = 0.2))
-x = hist(alt1, breaks = 20, plot = F)
+reps = 500000
+
+alt1 = replicate(reps, getP(n=13, effectSize = 0.2))
+x = hist(alt1, breaks = 15, plot = F)
 lines(x$mids, x$density, type = "both", pch = 1, col = "blue")
 
-alt2 = replicate(10000, getP(n=100, effectSize = 0.2))
-x = hist(alt2, breaks = 20, plot = F)
-lines(x$mids, x$density, type = "both", pch = 0, col = "red")
-
-alt3 = replicate(10000, getP(n=200, effectSize = 0.2))
-x = hist(alt3, breaks = 20, plot = F)
+alt2 = replicate(reps, getP(n=200, effectSize = 0.2))
+x = hist(alt3, breaks = 15, plot = F)
 lines(x$mids, x$density, type = "both", pch = 2, col = "orange")
 
-legend("topright", legend = c("null", "n = 15, power = 10%", "n = 100, power = 50%","n = 200, power = 80%"), lty = c(1,NA,NA,NA), pch = c(NA,1,0,2), col = c("black", "blue", "red","orange"))
+legend("topright", 
+       legend = c("H0 (no effect)", "H1, n = 13, power = ~10%", "H1, n = 200, power = ~80%"), 
+       lty = c(1,NA,NA), 
+       pch = c(NA,1,2), 
+       col = c("black", "blue", "orange"), 
+       bty = "n")
 
 
 
@@ -40,20 +44,26 @@ getResult <- function(n=10, effectSize = 0.2, sd = 1){
   return(c(p = p, probAlt = odds))
 }
 
-sim1 = as.data.frame(t(replicate(500, getResult(n = 10))))
-sim2 = as.data.frame(t(replicate(500, getResult(n = 20))))
-sim3 = as.data.frame(t(replicate(500, getResult(n = 100))))
+sim1 = as.data.frame(t(replicate(500, getResult(n = 13))))
+sim2 = as.data.frame(t(replicate(500, getResult(n = 200))))
 
 
-plot(probAlt ~ p, data = sim1, ylim = c(0,1), xlim = c(0,0.6), ylab = "Pr(H1|data)", xlab = "p-value", main = "BF vs. p-value")
-points(probAlt ~ p, data = sim2, col = "blue")
-points(probAlt ~ p, data = sim3, col = "red")
+plot(probAlt ~ p, data = sim1, ylim = c(0,1), xlim = c(0,0.6), ylab = "Pr(H1|data)", xlab = "p-value", main = "BF vs. p-value", col = "blue")
+points(probAlt ~ p, data = sim2, col = "orange", pch = 2)
 
 abline(h=0.5, lty = 2)
 
-legend("top", legend = c("n = 10", "n = 20", "n = 100"), pch = 1, col = c("black", "blue", "red"))
-text(0.5, 0.97, "H1 more likely", adj = 0.5)
-text(0.5, 0.03, "H0 more likely", adj = 0.5)
+legend("topright", 
+       legend = c("n = 13", "n = 200"), 
+       pch = c(1,2), 
+       col = c("blue", "orange"), 
+       bty = "n")
+text(0.6, 0.55, "H1 more likely", adj = 1)
+text(0.6, 0.45, "H0 more likely", adj = 1)
+
+
+mtext('text is here', side=1, line=3.5, at=9)
+mtext('text is here', side=1, line=3.5, at=9)
 
 dev.off()
 
